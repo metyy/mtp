ae31c6f4007af2fb35802cf388970b075ee502b8
System Administrator <root@Ahmet-MacBook-Pro.local>
2018-12-31 16:14:04 +0300

    main cpp toparlandi

--- met.cpp
+++ met.cpp
@@ -1,1 +1,19 @@
-selamlar dostlar
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+
+#define MTP "__MTP__"
+#define MTP_SLAYER "__MTP__SLAYER__02"
+
+/*
+Met Transport Protocol Service
+Version : v2.3 Slayer
+Loading...
+
+*\
+
+int main()
+{
+	std::cout<"LOADİNG...";
+}
